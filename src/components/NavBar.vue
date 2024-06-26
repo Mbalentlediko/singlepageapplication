097f03d6e17566deb4837d0ad59ac9d8577334e4
Mbalentlediko <dikombalentle9@gmail.com>
2024-06-26 11:02:53 +0200

    navBar Setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="nvabar-brand" to="/">
+        <img
+          src="https://mbalentlediko.github.io/Images/static/md logo.jpg"
+          alt="logo"
+          class="img-fluid"
+          loading="lazy"
+        />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only"></span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Resume</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Testimonials</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact me</a>
+          </li>
+        </ul>
+        
+         
+        
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style></style>
