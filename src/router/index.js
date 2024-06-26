03d4c0f20e1ef8470d3bf2eae396fdec51394be6
Mbalentlediko <dikombalentle9@gmail.com>
2024-06-26 16:23:03 +0200

    LandingPage Setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,66 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
+import axios from 'axios'
+import Swal from 'sweetalert2/dist/sweetalert2'
+const portfolioURL = 'https://mbalentlediko.github.io/eompTask/'
+export default createStore({
+  state: {
+    jobTitle: null,
+    aboutMe: null,
+    education:null,
+    experience: null,
+    skills: null,
+    testimonials: null,
+    projects: null
+  },
+  getters: {
+  },
+  mutations: {
+    setJobTitle(state, value){
+      state.jobTitle = value
+    },
+    setAboutMw(state, value){
+      state.aboutMe = value
+    },
+    setEducation(state, value){
+      state.education= value
+    },
+    setExperience(state, value){
+      state.experience = value
+    },
+    setSkills(state, value){
+      state.skills = value
+    },
+    setTestimonials(state, value){
+      state.testimonials = value
+    },
+    setProjects(state, value){
+      state.projects = value
+    }
+  },
+  actions: {
+    async fetchJobTitle(context) {
+      try{
+      let {jobTitle} =  await (await axios.get(portfolioURL)).data
+      context.commit ("setJobTitle", jobTitle)
+    }
+    catch(e) {
+      Swal.fire({
+        title: "Error",
+        text: "Failed to fetch data",
+        icon: "error",
+        timer:2000
 
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: ()=> import('@/views/HomeView.vue')
+      })
+    }
   }
-  
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  },
+  modules: {
+  }
 })
 
-export default router
+
+
+
+
+
+
