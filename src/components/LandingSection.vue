44e361df86fd718f55f9f1a2f82412a8ee3523a7
Mbalentlediko <dikombalentle9@gmail.com>
2024-06-27 09:42:49 +0200

    Setup

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row vh-100 align-items-center">
         <div class="col">
-            <img src="" alt="portfolio-picture" loading="lazy">
+            <img src="https://mbalentlediko.github.io/Images/static/you.jpg" alt="portfolio-picture" loading="lazy">
           
         </div>
         <div class="col">
         <div class="details">
             <h1 class="display-1">Mbalentle Diko</h1>
-            <span> </span>
+            <p v-if="title">
+              I am
+              <span>{{ title }} </span>
+            </p>
         </div>
       </div>
     </div>
@@ -16,11 +19,30 @@
 </template>
 
 <script setup>
-import {computed, onMounted } from "vue"
+//Displays job title within an array , just like an animatuon 
+import {computed, onMounted, ref} from "vue"
+import {useStore} from 'vuex'
+const store = useStore()
 const JobTitle = computed(()=> this.$store.state.JobTitle) 
-onMounted(() =>{ this.dispatch('fetchJobTitle')
-  
-})
+const title = ref('a software developer')
+const cnt = ref(0)
+
+ function repeat() {
+  try {
+    if (cnt.value == JobTitle.value?.length ) cnt.value = 0;
+    title.value =
+    JobTitle.value?.at(cnt.value)?.title;
+    setTimeout(repeat,2000)
+    cnt.value++
+  } catch(e) {
+//
+
+  }
+ }
+ onMounted(()=> {
+  store.dispatch('fetchJobTitle')
+  repeat()
+ })
 
 </script>
 
